00b2a72f55cc07336a1e6419a4505080f314ebd9
furkansezer0 <furkan-sezer@outlook.com>
2025-02-10 02:16:58 +0300

    created a vue project for projects portfolio

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/Home.vue'
+import ProjectsView from '../views/Projects.vue'
+import AboutView from '../views/About.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: ProjectsView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Welcome to My Portfolio</h1>
+    <div class="intro">
+      <h2>Hi, I'm Sezer Furkan</h2>
+      <p>I'm a software developer passionate about creating amazing web applications.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
+<style scoped>
+.home {
+  padding: 2rem;
+}
+.intro {
+  max-width: 800px;
+  margin: 2rem auto;
+  text-align: left;
+}
+</style>
