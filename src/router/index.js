071281d7ce1f3cde2a1b97d4b6bbf01fa5439b93
furkan.sezer <furkan.sezer@vidmetrica.com>
2025-02-10 14:18:58 +0300

    deploy after the build

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/sezerfurkan.github.io/'),
+  history: createWebHistory(''),
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/sezerfurkan.github.io/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? ''  // Changed from '/sezerfurkan.github.io/' to empty string
+    : '/'
 })
